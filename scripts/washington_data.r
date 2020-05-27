c2e5ac57c3db934bbf0db6f0319526f449abc4fa
Chiaki <cmnicilover@gmail.com>
2020-05-26 21:09:25 -0700

    remove scatterplot line

--- scripts/washington_data.r
+++ scripts/washington_data.r
@@ -13,7 +13,6 @@ claims_data$Initial.Claims <- gsub(",", "", claims_data$Initial.Claims)
 claims_data$Initial.Claims <- as.numeric(claims_data$Initial.Claims)
 claims_plot <- ggplot(data = claims_data, aes(x = Week, y = Initial.Claims,
                                               group = 1)) +
-  geom_line() +
   geom_point() +
   scale_y_continuous(limits = c(5000, 185000)) +
   labs(
